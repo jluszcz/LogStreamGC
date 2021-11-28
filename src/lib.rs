db3508a16b72a7e613d4ec4bf304776e2c0f3a2e
Jacob Luszcz <jacob.luszcz@gmail.com>
2021-11-28 08:27:18 -0500

    Suppress WARN logs from aws_config::profile

--- src/lib.rs
+++ src/lib.rs
@@ -30,6 +30,7 @@ where
             ))
         })
         .level(LevelFilter::Warn)
+        .level_for("aws_config::profile", LevelFilter::Error)
         .level_for("log_stream_gc", level)
         .level_for(calling_module, level)
         .chain(std::io::stdout())
