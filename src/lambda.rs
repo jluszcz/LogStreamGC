976d470c0c02c3d2f82388b20c1cb053ecb24ea9
Jacob Luszcz <jacob.luszcz@gmail.com>
2025-05-06 10:09:38 -0400

    Log rustc version on Lambda runs

--- src/lambda.rs
+++ src/lambda.rs
@@ -1,9 +1,12 @@
 use lambda_runtime::{LambdaEvent, service_fn};
+use log::info;
 use log_stream_gc::{gc_log_streams, set_up_logger};
 use serde_json::{Value, json};
 use std::error::Error;
 
 type LambdaError = Box<dyn Error + Send + Sync + 'static>;
+
+const RUSTC_VERSION: &str = env!("RUSTC_VERSION");
 
 #[tokio::main]
 async fn main() -> Result<(), LambdaError> {
@@ -14,6 +17,8 @@ async fn main() -> Result<(), LambdaError> {
 
 async fn function(_event: LambdaEvent<Value>) -> Result<Value, LambdaError> {
     set_up_logger(module_path!(), false)?;
+
+    info!("rustc version: {RUSTC_VERSION}");
 
     gc_log_streams(None, false).await?;
 
